4d1fd607bdf4fa119b71232c2443721984d6fc80
katemanson <katemanson107@hotmail.com>
2016-12-30 12:02:17 +0000

    temporarily moves grid functionality into index.js, to make grid data available during game play

--- client/src/grid.js
+++ client/src/grid.js
@@ -1,66 +1,70 @@
-var Grid = function(){
-  this.makeGrid();
-}
-
-Grid.prototype = {
-
-  dragOverHandler: function(event){
-    if (event.target.tagName !== "CANVAS" && event.target.childElementCount === 0){
-      event.preventDefault();
-    }
-  },
-
-  dropHandler: function(event){
-    if (event.target.tagName !== "CANVAS" && event.target.childElementCount === 0){
-      event.preventDefault();
-      var data = event.dataTransfer.getData("text");
-      var tile = JSON.parse(data);
-      tile.gridPosition = event.target.id;
-      //? Bit weird: the console log immediately below gives the element id updated
-      //with the new grid position (but this update doesn't happen til later on in the method).
-      //Is this to do with JS asynchronous-ness?
-      // console.log('document.getElementById(data)', document.getElementById(data));
-      var draggedElement = document.getElementById(data);
-      // !!ToDo!! Need to update hand with tile in new position.
-      event.target.appendChild(draggedElement);
-      draggedElement.id = JSON.stringify(tile);
-      console.log('drop event target after updating tile canvas id: ', event.target);
-    }
-  },
-
-  makeGrid: function(){
-    var scroller = document.scrollingElement;
-    var scrollWidth = scroller.scrollWidth;
-    var scrollHeight = scroller.scrollHeight;
-    var numberColumns = Math.ceil(scrollWidth/49);
-    var numberRows = Math.ceil(scrollHeight/49);
-    var grid = document.getElementById('grid');
-
-    for (var i = 0; i < numberRows; i++){
-      var row = document.createElement('tr');
-      grid.appendChild(row);
-
-      for (var j = 0; j < numberColumns; j++){
-        var square = document.createElement('td');
-        square.id = "c" + j + "r" + i;
-        square.ondragover = this.dragOverHandler;
-        square.ondrop = this.dropHandler;
-        row.appendChild(square);
-      }
-    }
-    console.log('scroller.scrollWidth', scroller.scrollWidth);
-    console.log('window.innerWidth', window.innerWidth);
-    console.log('scroller.scrollWidth - window.innerWidth', scroller.scrollWidth - window.innerWidth);
-    console.log('scroller.scrollLeft', scroller.scrollLeft);
-
-    this.scrollGrid(scroller, grid);
-  },
-
-  scrollGrid: function(scroller, grid){
-    if (scroller.scrollWidth - window.innerWidth >= scroller.scrollLeft){
-      // console.log('in scrollGrid if statement');
-    }
-  }
-}
-
-module.exports = Grid;
+// var Player = require('./player');
+// //? Don't like importing Player here; feels messy.
+//
+// var Grid = function(){
+//   this.makeGrid();
+// }
+//
+// Grid.prototype = {
+//
+//   dragOverHandler: function(event){
+//     if (event.target.tagName !== "CANVAS" && event.target.childElementCount === 0){
+//       event.preventDefault();
+//     }
+//   },
+//
+//   dropHandler: function(event){
+//     if (event.target.tagName !== "CANVAS" && event.target.childElementCount === 0){
+//       event.preventDefault();
+//       var data = event.dataTransfer.getData("text");
+//       var tile = JSON.parse(data);
+//       tile.gridPosition = event.target.id;
+//       //? Bit weird: the console log immediately below gives the element id updated
+//       //with the new grid position (but this update doesn't happen 'til later on in the method).
+//       //Is this to do with JS asynchronous-ness?
+//       // console.log('document.getElementById(data)', document.getElementById(data));
+//       var draggedElement = document.getElementById(data);
+//
+//       // !!ToDo!! Need to update hand with tile in new position.
+//       event.target.appendChild(draggedElement);
+//       draggedElement.id = JSON.stringify(tile);
+//       console.log('drop event target after updating tile canvas id: ', event.target);
+//     }
+//   },
+//
+//   makeGrid: function(){
+//     var scroller = document.scrollingElement;
+//     var scrollWidth = scroller.scrollWidth;
+//     var scrollHeight = scroller.scrollHeight;
+//     var numberColumns = Math.ceil(scrollWidth/49);
+//     var numberRows = Math.ceil(scrollHeight/49);
+//     var grid = document.getElementById('grid');
+//
+//     for (var i = 0; i < numberRows; i++){
+//       var row = document.createElement('tr');
+//       grid.appendChild(row);
+//
+//       for (var j = 0; j < numberColumns; j++){
+//         var square = document.createElement('td');
+//         square.id = "c" + j + "r" + i;
+//         square.ondragover = this.dragOverHandler;
+//         square.ondrop = this.dropHandler;
+//         row.appendChild(square);
+//       }
+//     }
+//     console.log('scroller.scrollWidth', scroller.scrollWidth);
+//     console.log('window.innerWidth', window.innerWidth);
+//     console.log('scroller.scrollWidth - window.innerWidth', scroller.scrollWidth - window.innerWidth);
+//     console.log('scroller.scrollLeft', scroller.scrollLeft);
+//
+//     this.scrollGrid(scroller, grid, i, j);
+//   },
+//
+//   scrollGrid: function(scroller, grid, i, j){
+//     if (scroller.scrollWidth - window.innerWidth >= scroller.scrollLeft){
+//       // console.log('in scrollGrid if statement');
+//     }
+//   }
+// }
+//
+// module.exports = Grid;
--- client/src/index.js
+++ client/src/index.js
@@ -1,35 +1,106 @@
+// var Grid = require('./grid');
+var Game = require('./game');
+var Player = require('./player');
 var Tile = require('./tile');
 var TileBank = require('./tile_bank');
-var Grid = require('./grid');
 
 window.onload = function(){
 
-  var grid = new Grid();
-  var hand = tileBank.takeHand(21);
+  var game = new Game();
+  var player = new Player("Vyvyan");
+  game.addPlayer(player);
+  game.dealHand(21);
+  console.log('player hand:', player.hand);
 
   var dragStartHandler = function(event){
     event.dataTransfer.setData("text", event.target.id);
     event.target.opacity = 0.5;
   }
 
-  //ToDo: Below assumes there will be at least as many grid squares in
-  //the first row as there are tiles in the hand; when that's the case it
-  //throws an error and stops adding tiles to the grid.
-  for (var i = 0; i < hand.length; i++){
-    var gridSquare = document.getElementById('grid').children[0].children[i];
-    var canvas = document.createElement('canvas');
-    var tile = hand[i];
-    tile.gridPosition = gridSquare.id;
-    var tileJSON = JSON.stringify(tile);
-    var context = canvas.getContext('2d');
-
-    canvas.id = tileJSON;
-    canvas.draggable = true;
-    canvas.width = 45;
-    canvas.height = 45;
-    canvas.ondragstart = dragStartHandler;
-    gridSquare.appendChild(canvas);
-    tile.draw(context);
+  var dragOverHandler = function(event){
+    if (event.target.tagName !== "CANVAS" && event.target.childElementCount === 0){
+      event.preventDefault();
+    }
   }
+
+  var dropHandler = function(event){
+    if (event.target.tagName !== "CANVAS" && event.target.childElementCount === 0){
+      event.preventDefault();
+      var data = event.dataTransfer.getData("text");
+      var tile = JSON.parse(data);
+      tile.gridPosition = event.target.id;
+      //? Bit weird: the console log immediately below gives the element id updated
+      //with the new grid position (but this update doesn't happen 'til later on in the method).
+      //Is this to do with JS asynchronous-ness?
+      // console.log('document.getElementById(data)', document.getElementById(data));
+      var draggedElement = document.getElementById(data);
+
+      // !!ToDo!! Need to update hand with tile in new position.
+      event.target.appendChild(draggedElement);
+      draggedElement.id = JSON.stringify(tile);
+      console.log('drop event target after updating tile canvas id: ', event.target);
+    }
+  }
+
+  var makeGrid = function(){
+    var scroller = document.scrollingElement;
+    var scrollWidth = scroller.scrollWidth;
+    var scrollHeight = scroller.scrollHeight;
+    var numberColumns = Math.ceil(scrollWidth/49);
+    var numberRows = Math.ceil(scrollHeight/49);
+    var grid = document.getElementById('grid');
+
+    for (var i = 0; i < numberRows; i++){
+      var row = document.createElement('tr');
+      grid.appendChild(row);
+
+      for (var j = 0; j < numberColumns; j++){
+        var square = document.createElement('td');
+        square.id = "c" + j + "r" + i;
+        square.ondragover = dragOverHandler;
+        square.ondrop = dropHandler;
+        row.appendChild(square);
+      }
+    }
+    // console.log('scroller.scrollWidth', scroller.scrollWidth);
+    // console.log('window.innerWidth', window.innerWidth);
+    // console.log('scroller.scrollWidth - window.innerWidth', scroller.scrollWidth - window.innerWidth);
+    // console.log('scroller.scrollLeft', scroller.scrollLeft);
+
+    //ToDo: scrollGrid function to make infinite scrolling grid.
+    // this.scrollGrid(scroller, grid, i, j);
+  }
+
+  //ToDo: scrollGrid function to make infinite scrolling grid.
+  // var scrollGrid = function(scroller, grid, i, j){
+  //   if (scroller.scrollWidth - window.innerWidth >= scroller.scrollLeft){
+  //     // console.log('in scrollGrid if statement');
+  //   }
+  // }
+
+  var showHand = function(){
+    //ToDo: Below assumes there will be at least as many grid squares in
+    //the first row as there are tiles in the hand; when that's the case it
+    //throws an error and stops adding tiles to the grid.
+    for (var i = 0; i < player.hand.length; i++){
+      var gridSquare = document.getElementById('grid').children[0].children[i];
+      var canvas = document.createElement('canvas');
+      var tile = player.hand[i];
+      tile.gridPosition = gridSquare.id;
+      var tileJSON = JSON.stringify(tile);
+      var context = canvas.getContext('2d');
+
+      canvas.id = tileJSON;
+      canvas.draggable = true;
+      canvas.width = 45;
+      canvas.height = 45;
+      canvas.ondragstart = dragStartHandler;
+      gridSquare.appendChild(canvas);
+      tile.draw(context);
+    }
+  }
+
+  makeGrid();
+  showHand();
 
 }
